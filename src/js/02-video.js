25440a268da20feeaebbad43b57ac02f8906cb1f
Pavel Lobtsov <plobtsov@gmail.com>
2022-04-05 17:45:41 +0300

    task 2 finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(currentTimeUpdate, 1000));
+function currentTimeUpdate({ seconds }) {
+  localStorage.setItem(currentTime, seconds);
+}
+player.setCurrentTime(localStorage.getItem(currentTime));
